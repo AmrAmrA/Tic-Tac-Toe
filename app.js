40eed7cd7da90f815ef4dd9ce2c65a947d981be8
Abd El Fattah Amr <amrelfattah.dev@gmail.com>
2022-12-09 08:32:16 +0100

    Add a reset function

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ let messagePlayer = document.querySelector(".playPlease");
 
 let buttonReset = document.querySelector(".reset");
 let yourTurn = 1;
-
+let count = 0; 
 
 
 
 for (const casePlay of gameCase) {
   casePlay.addEventListener("click", changeImage);
   function changeImage() {
+    count++
+    console.log(count);
+    // console.log(count);
     if (yourTurn === 1 && casePlay.src === "http://127.0.0.1:5500/square.png") {
       casePlay.src = "cross.jpg";
       casePlay.classList.add("actif");
@@ -34,7 +37,11 @@ for (const casePlay of gameCase) {
 for (let i = 0; i < gameCase.length; i++) {
   buttonReset.addEventListener('click', resetImage)
   function resetImage() {
-  if (gameCase[i].classList.contains("actif")) {
-    gameCase[i].src = "http://127.0.0.1:5500/square.png";
-  }}
-}
+    if (count = 9) {
+      gameCase[i].src = "http://127.0.0.1:5500/square.png";
+      count = 0; 
+    }
+  
+  }
+  }
+     
