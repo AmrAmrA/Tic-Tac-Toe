b970c035ba3c0f0252a08311d6228026c469e637
Abd El Fattah Amr <amrelfattah.dev@gmail.com>
2022-12-10 12:24:37 +0100

    Fix Reset Function Bug

--- app.js
+++ app.js
@@ -5,43 +5,38 @@ let messagePlayer = document.querySelector(".playPlease");
 
 let buttonReset = document.querySelector(".reset");
 let yourTurn = 1;
-let count = 0; 
+let count = 0;
 
-
-
-for (const casePlay of gameCase) {
+for (let casePlay of gameCase) {
   casePlay.addEventListener("click", changeImage);
   function changeImage() {
-    count++
-    console.log(count);
-    // console.log(count);
     if (yourTurn === 1 && casePlay.src === "http://127.0.0.1:5500/square.png") {
       casePlay.src = "cross.jpg";
-      casePlay.classList.add("actif");
       yourTurn--;
       messagePlayer.textContent = `C'est au tour du joueur O `;
+      count++;
+      console.log(count);
+      console.log(casePlay.classList[1]);
     } else if (
       yourTurn === 0 &&
       casePlay.src === "http://127.0.0.1:5500/square.png"
     ) {
       casePlay.src = "circle.png";
-      casePlay.classList.add("actif");
       messagePlayer.textContent = `C'est au tour du joueur X `;
       yourTurn++;
+      count++;
+      console.log(count);
+      console.log(casePlay.classList[1]);
     }
   }
 }
 
-
-
-for (let i = 0; i < gameCase.length; i++) {
-  buttonReset.addEventListener('click', resetImage)
-  function resetImage() {
-    if (count = 9) {
-      gameCase[i].src = "http://127.0.0.1:5500/square.png";
-      count = 0; 
+buttonReset.addEventListener("click", resetImage);
+function resetImage() {
+  if (count == 9) {
+    for (let individualCase of gameCase) {
+      individualCase.src = "http://127.0.0.1:5500/square.png";
+      count = 0;
     }
-  
   }
-  }
-     
+}
